91b398e7d43809a949cc9a3cf522ebee46514080
Mavin Sao <mavinsao@Mavins-Mac-mini.local>
2021-09-17 15:28:41 +0700

    add icon app

--- iChat/Cell/RoomTableViewCell.swift
+++ iChat/Cell/RoomTableViewCell.swift
@@ -19,8 +19,22 @@ class RoomTableViewCell: UITableViewCell {
     func config(room: PrivateRoom){
         let currID = defaults.string(forKey: "currentID")
         
-        let pfURLOne = URL(string: room.userOneAvatar!)
-        let pfURLTwo = URL(string: room.userTwoAvatar!)
+ 
+        
+        var avatarURLOne = "https://cliply.co/wp-content/uploads/2020/08/442008112_GLANCING_AVATAR_3D_400.png"
+        var avatarURLTwo = "https://cliply.co/wp-content/uploads/2020/08/442008112_GLANCING_AVATAR_3D_400.png"
+        
+        if let avatarOne =  room.userOneAvatar, let avatarTwo = room.userTwoAvatar{
+            if avatarOne != ""{
+                avatarURLOne = avatarOne
+            }
+            if avatarTwo != ""{
+                avatarURLTwo = avatarTwo
+            }
+        }
+        
+        let pfURLOne = URL(string: avatarURLOne)
+        let pfURLTwo = URL(string: avatarURLTwo)
         
         if room.membersId![0] == currID {
             self.usernameLabel.text = room.userTwoName
